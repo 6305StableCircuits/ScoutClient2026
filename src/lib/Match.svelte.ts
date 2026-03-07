93d9a7588b77689dae95f2aba65067472b90daba
selenavi <selenavi001@gmail.com>
2026-03-07 14:56:17 -0800

    Update Match.svelte.ts

--- src/lib/Match.svelte.ts
+++ src/lib/Match.svelte.ts
@@ -5,7 +5,7 @@ import Timer, { sleep } from '$lib/Timer.svelte';
 export default class Match {
     static State = class State {
         match: Match;
-        timer = new Timer(DEV ? '0:20' : '2:40');
+        timer = new Timer(DEV ? '0:20' : '2:50');
         state = $state<'pre' | 'auto' | 'teleop' | 'post'>('pre');
         start() {
             this.match.date = Date.now();
