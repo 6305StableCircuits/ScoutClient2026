8ce69c8a6d8a0811acdf7797d92788218783f7cc
selenavi <selenavi001@gmail.com>
2026-03-28 10:55:52 -0700

    autonotes fix

--- src/lib/Match.svelte.ts
+++ src/lib/Match.svelte.ts
@@ -147,7 +147,6 @@ export default class Match {
             teleop: object;
             accuracy: object;
         };
-        autonotes: string | null;
         notes: string | null;
         assists: number;
     }) {
@@ -168,11 +167,11 @@ export default class Match {
             teleop: object;
             accuracy: object;
         };
-        autonotes: string | null;
+
         notes: string | null;
         assists: number;
     } {
-        const { team, match, date, scout, alliance, score, autonotes, notes, assists } = this;
+        const { team, match, date, scout, alliance, score, notes, assists } = this;
         const { overall, auto, teleop, accuracy } = score;
         return {
             team,
@@ -180,7 +179,6 @@ export default class Match {
             date,
             scout,
             alliance,
-            autonotes,
             notes,
             assists,
             score: {
@@ -195,7 +193,6 @@ export default class Match {
     match = $state(0);
     date = $state(0);
     scout = $state('');
-    autonotes = $state<string | null>(null);
     notes = $state<string | null>(null);
     alliance = $state<'red' | 'blue'>('red');
     score = new Match.Scoring();
@@ -207,7 +204,6 @@ export default class Match {
             team: this.team,
             date: this.date,
             alliance: this.alliance,
-            autonotes: this.autonotes,
             notes: this.notes,
             assists: this.assists
         };
